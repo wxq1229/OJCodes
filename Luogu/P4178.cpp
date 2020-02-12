482aaeccef4fd44b6f2220548a5572692048d0bb
wxq1229 <674068128@qq.com>
2020-02-12 16:17:25 +0800

    UPDATE Luogu

--- Luogu/P4178.cpp
+++ Luogu/P4178.cpp
@@ -1,86 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,j,k) for (int i=(int)(j);i<=(int)(k);i++)
-#define per(i,j,k) for (int i=(int)(j);i>=(int)(k);i--)
-#define mp make_pair
-#define pb push_back
-#define fi first
-#define se second
-#define MAXN 100010
-int head[MAXN],cnt=0;
-struct edge{
-	int to,w,nxt;
-}e[MAXN<<1];
-void add(int x,int y,int w){e[++cnt]=(edge){y,w,head[x]};head[x]=cnt;}
-void addedge(int x,int y,int w){add(x,y,w);add(y,x,w);}
-int root,sum,n,K,dp[MAXN],size[MAXN],vis[MAXN],dis[MAXN];
-void getRoot(int x,int fa){
-	size[x]=1;dp[x]=0;
-	for (int i=head[x];i;i=e[i].nxt){
-		int v=e[i].to;
-		if (v==fa||vis[v])continue;
-		getRoot(v,x);
-		size[x]+=size[v];
-		dp[x]=max(dp[x],size[v]);
+#define fore(i,x) for(int i=head[x],v=e[i].to;i;i=e[i].nxt,v=e[i].to)
+const int N=1e5+10;
+int n,K;
+struct edge
+{
+	int to,nxt,w;
+}e[N<<1];
+int head[N],cnt=0;
+inline void ade(int x,int y,int w)
+{e[++cnt]=(edge){y,head[x],w},head[x]=cnt;}
+inline void addedge(int x,int y,int w){ade(x,y,w),ade(y,x,w);}
+int siz[N],mx[N],rt,all,vis[N];
+void getrt(int x,int prev)
+{
+	siz[x]=1,mx[x]=0;
+	fore(i,x)if(!vis[v]&&v!=prev)
+	{
+		getrt(v,x),siz[x]+=siz[v];
+		mx[x]=max(mx[x],siz[v]);
 	}
-	dp[x]=max(dp[x],sum-size[x]);
-	if (dp[x]<dp[root]) root=x;
+	mx[x]=max(mx[x],all-siz[x]);
+	if(mx[x]<mx[rt]) rt=x;
 }
-int d[MAXN],tot;
-void getDis(int x,int fa){
-	d[++tot]=dis[x];
-	for (int i=head[x];i;i=e[i].nxt){
-		int v=e[i].to;
-		if (v==fa||vis[v])continue;
-		dis[v]=dis[x]+e[i].w;
-		getDis(v,x);
+int dis[N],tot;
+void getd(int x,int prev,int d)
+{
+	dis[++tot]=d;
+	fore(i,x) if(v!=prev&&!vis[v]) getd(v,x,d+e[i].w);
+}
+int calc(int x,int w)
+{
+	tot=0,getd(x,0,w);
+	sort(dis+1,dis+tot+1);
+	int nw=tot,ans=0; for(int i=1;i<=tot;i++)
+	{
+		while(dis[nw]+dis[i]>K) nw--;
+		if(nw<=i) break; ans+=nw-i;
 	}
+	return ans;
 }
-int calc(int x,int w){
-	tot=0;dis[x]=w;getDis(x,0);
-	sort(d+1,d+tot+1);
-	int l=1,r=tot,res=0;
-	while (l<=r){
-		if (d[l]+d[r]<=K)res+=r-l,l++;
-		else r--;
-	}
-	return res;
-}
-int ans=0;
-void solve(int x){
-	vis[x]=1;ans+=calc(x,0);
-	for (int i=head[x];i;i=e[i].nxt){
-		int v=e[i].to;
-		if (vis[v])continue;
-		ans-=calc(v,e[i].w);
-		dp[root=0]=n,sum=size[v];getRoot(v,x);
-		solve(root);
+int ans;
+void dfz(int x)
+{
+	vis[x]=1,ans+=calc(x,0);
+	fore(i,x) if(!vis[v]) ans-=calc(v,e[i].w);
+	fore(i,x) if(!vis[v])
+	{
+		rt=0,all=mx[rt]=siz[v];
+		getrt(v,x),dfz(rt);
 	}
 }
-int main(){
+int main()
+{
 	scanf("%d",&n);
-	rep (i,1,n-1){
-		int x,y,w;scanf("%d%d%d",&x,&y,&w);
-		addedge(x,y,w);
-	}
-	dp[root=0]=sum=n;
+	for(int i=1,x,y,w;i<=n-1;i++)
+		scanf("%d%d%d",&x,&y,&w),addedge(x,y,w);
 	scanf("%d",&K);
-	getRoot(1,0);solve(root);
+	rt=0,all=mx[rt]=n;
+	getrt(1,0),dfz(rt);
 	printf("%d\n",ans);
 	return 0;
 }
-/*
-7
-1 6 13 
-6 3 9 
-3 5 7 
-4 1 3 
-2 4 20 
-4 7 2 
-10
-*/
-
-
-
-
-

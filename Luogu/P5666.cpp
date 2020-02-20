54d62b6f6972c9f04b78a6c187a6244a75110cbd
wxq1229 <674068128@qq.com>
2020-02-20 17:28:13 +0800

    update Luogu

---
+++ Luogu/P5666.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=0;i--)
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef long long ll;
+typedef double db;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+
+const int N=1e5+10;
+
+int main() {
+    return 0;
+}
